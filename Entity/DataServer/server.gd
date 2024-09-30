8b2a67306b26fc0dceef65de989d510833f70ed8
Barys <barys.radzko@protonmail.com>
2024-10-01 00:14:45 +0200

    Pattern Constructors improved

--- Entity/DataServer/server.gd
+++ Entity/DataServer/server.gd
@@ -36,12 +36,17 @@ var affiliation : CharacterBody3D = null : set = affiliation_changed
 var affiliation_id : String = "" : set = affiliation_id_changed
 var health : int = HEALTH : set = health_changed
 
-var bullet_rotation : int = 0
+var bullet_rotation : float = 0
+var bullet_rotation_alt : float = 0
+
 var direction_changed : bool = false
+var direction_changed_alt : bool = false
+
 var attack_mode : int = 0
 
 var enemy_in_area : Array
 var enemy : CharacterBody3D
+var init_rotation : float = 0
 
 
 #	[ Setters ]
@@ -94,11 +99,12 @@ func attack_with_pattern(pattern : int):
 		0:
 			pass
 		1:	# Normal Bullet Stream
-			twisted_stream(0.005, 60, 120, 12, 0, 5.0)
+			twisted_N_stream(0.005, 60, 120, 12, 0, 5.0, 1, false)
 		2:	# Easy Bullet Stream
-			twisted_stream(0.01, 10, 30, 10, 20, 5.0)
+			twisted_N_stream(0.01, 10, 30, 10, 20, 5.0, 1, false)
 		3:	# Easy Bullet 4Stream
-			twisted_4stream(0.2, 5, 5, 15, 10, 3.5)
+			twisted_N_stream(0.1, 5, 6, 8, 0, 3.5, 8, false)
+			twisted_N_stream(0.1, 5, 6, 10, 0, 3.5, 8, true)
 		4:	# Easy Targeted bullet stream
 			targeted_bullet(0.1, 3, 3, 25, 0, 15, 1, 1)
 
@@ -106,90 +112,45 @@ func attack_with_pattern(pattern : int):
 
 #	[ BulletHell patterns ]
 
-#	just a stream
-func stream(wait_time : float, bullet_speed : int, bullet_live_time : float):
+#	Stream of bullets, mostly a tool.
+func stream(bullet_speed : int, bullet_live_time : float, additional_init_rotation : float, is_alternative : bool):
 	
 	var bullet = ROUND_BULLET.instantiate()
 	bullet.position = global_position
-	bullet.rotation.y = deg_to_rad(bullet_rotation)
+	if is_alternative:
+		bullet.rotation.y = deg_to_rad(additional_init_rotation) + deg_to_rad(bullet_rotation_alt)
+	else:
+		bullet.rotation.y = deg_to_rad(additional_init_rotation) + deg_to_rad(bullet_rotation)
+		
 	bullet.name = "ServerBullet_"
 	bullet.bullet_initiate(bullet_speed, bullet_live_time)
 	get_parent().add_child(bullet, true)
 
-#	just one stream of bullets
-func twisted_stream(wait_time : float, rand_angle_from : int, rand_angle_to : int, bullet_speed : int, random_change_direction : int, bullet_live_time : float):
+#	N Streams of bullets
+func twisted_N_stream(wait_time : float, rand_angle_from : int, rand_angle_to : int, bullet_speed : int, random_change_direction : int, bullet_live_time : float, number_of_streams : int, is_alternative : bool):
 	
-	rotate_projectile_source(wait_time, rand_angle_from, rand_angle_to, random_change_direction)
-	
-	stream(wait_time, bullet_speed, bullet_live_time)
-
-
-#	N Streams of bullets every next one is under 90.
-func twisted_N_stream(number_of_streams : int, wait_time : float, rand_angle_from : int, rand_angle_to : int, bullet_speed : int, random_change_direction : int, bullet_live_time : float):
-	
-	rotate_projectile_source(wait_time, rand_angle_from, rand_angle_to, random_change_direction)
+	rotate_projectile_source(wait_time, rand_angle_from, rand_angle_to, random_change_direction, is_alternative)
 	
 	if number_of_streams == 0:
-		twisted_stream(wait_time, rand_angle_from, rand_angle_to, bullet_speed, random_change_direction, bullet_live_time)
+		return
 	
-	var angle_step = 360 / number_of_streams
+	var additional_init_rotation : float = 360.0 / number_of_streams
 	
-	## TODO make it!
+	for i in range(number_of_streams):
+		stream(bullet_speed, bullet_live_time, init_rotation, is_alternative)
+		init_rotation += additional_init_rotation
+		if init_rotation >= 360:
+			init_rotation -= 360
 	
-	## Calculate angle step for evenly spaced streams
-	#var angle_step = 360.0 / number_of_streams
-	#
-	## Loop to fire bullets for each stream
-	#for i in range(number_of_streams):
-		## Calculate the angle for the current stream
-		#var stream_angle = angle_step * i
-		## Rotate the bullet to the calculated angle
-		#var bullet = ROUND_BULLET.instantiate()
-		#bullet.position = global_position
-		#
-		## Apply rotation for each stream's bullet
-		#bullet.rotation.y = deg_to_rad(stream_angle)
-		#bullet.name = "ServerBullet_" + str(i)
-		#bullet.bullet_initiate(bullet_speed, bullet_live_time)
-		#get_parent().add_child(bullet, true)
+	init_rotation = 0
 
-#	4 Streams of bullets every next one is under 90.
-func twisted_4stream(wait_time : float, rand_angle_from : int, rand_angle_to : int, bullet_speed : int, random_change_direction : int, bullet_live_time : float):
-	
-	rotate_projectile_source(wait_time, rand_angle_from, rand_angle_to, random_change_direction)
-	
-	var bullet1 = ROUND_BULLET.instantiate()
-	bullet1.position = global_position
-	bullet1.rotation.y = deg_to_rad(bullet_rotation)
-	bullet1.name = "ServerBullet_"
-	bullet1.bullet_initiate(bullet_speed, bullet_live_time)
-	get_parent().add_child(bullet1, true)
-	
-	var bullet2 = ROUND_BULLET.instantiate()
-	bullet2.position = global_position
-	bullet2.rotation.y = deg_to_rad(90) + deg_to_rad(bullet_rotation)
-	bullet2.name = "ServerBullet_"
-	bullet2.bullet_initiate(bullet_speed, bullet_live_time)
-	get_parent().add_child(bullet2, true)
-	
-	var bullet3 = ROUND_BULLET.instantiate()
-	bullet3.position = global_position
-	bullet3.rotation.y = deg_to_rad(180) + deg_to_rad(bullet_rotation)
-	bullet3.name = "ServerBullet_"
-	bullet3.bullet_initiate(bullet_speed, bullet_live_time)
-	get_parent().add_child(bullet3, true)
-	
-	var bullet4 = ROUND_BULLET.instantiate()
-	bullet4.position = global_position
-	bullet4.rotation.y = deg_to_rad(270) + deg_to_rad(bullet_rotation)
-	bullet4.name = "ServerBullet_"
-	bullet4.bullet_initiate(bullet_speed, bullet_live_time)
-	get_parent().add_child(bullet4, true)
 
 #	stream of bullets, that are aiming at enemy
 func targeted_bullet(wait_time : float, rand_angle_from : int, rand_angle_to : int, bullet_speed : int, random_change_direction : int, bullet_live_time : float, states_change_time_from : float, states_change_time_to : float):
 	if affiliation != null and enemy != null:
-		rotate_projectile_source(wait_time, rand_angle_from, rand_angle_to, random_change_direction)
+		
+		# Maybe it will be changed on for example circle spawn?
+		rotate_projectile_source(wait_time, rand_angle_from, rand_angle_to, random_change_direction, false)
 		
 		var bullet = BULLET_TARGETED.instantiate()
 		bullet.position = global_position
@@ -199,7 +160,8 @@ func targeted_bullet(wait_time : float, rand_angle_from : int, rand_angle_to : i
 		get_parent().add_child(bullet, true)
 
 #	func that rotates projectile source
-func rotate_projectile_source(wait_time : float, rand_angle_from : int, rand_angle_to : int, random_change_direction : int):
+func rotate_projectile_source(wait_time : float, rand_angle_from : int, rand_angle_to : int, random_change_direction : int, is_alternative : bool):
+	if not is_alternative:
 		if random_change_direction == 0:
 			pass
 		elif randi_range(0,random_change_direction) == random_change_direction:
@@ -209,14 +171,33 @@ func rotate_projectile_source(wait_time : float, rand_angle_from : int, rand_ang
 				direction_changed = false 
 		
 		if not direction_changed:
-			bullet_rotation += randi_range(rand_angle_from, rand_angle_to)
+			bullet_rotation += randf_range(rand_angle_from, rand_angle_to)
 		else:
-			bullet_rotation -= randi_range(rand_angle_from, rand_angle_to)
+			bullet_rotation -= randf_range(rand_angle_from, rand_angle_to)
 		
 		if bullet_rotation >= 360:
 			bullet_rotation = bullet_rotation - 360
 		elif bullet_rotation <= -360:
 			bullet_rotation = bullet_rotation + 360
+		attack_timer.start(wait_time)
+	elif is_alternative:
+		if random_change_direction == 0:
+			pass
+		elif randi_range(0,random_change_direction) == random_change_direction:
+			if direction_changed_alt == false:
+				direction_changed_alt = true
+			elif direction_changed_alt == true:
+				direction_changed_alt = false 
+		
+		if not direction_changed_alt:
+			bullet_rotation_alt -= randf_range(rand_angle_from, rand_angle_to)
+		else:
+			bullet_rotation_alt += randf_range(rand_angle_from, rand_angle_to)
+		
+		if bullet_rotation_alt >= 360:
+			bullet_rotation_alt = bullet_rotation_alt - 360
+		elif bullet_rotation_alt <= -360:
+			bullet_rotation_alt = bullet_rotation_alt + 360
 		attack_timer.start(wait_time)
 
 #	[ Child Node's signals ]
